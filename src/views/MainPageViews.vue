277c00332d2bc3c0c6331c22d758318551a4f0d6
Thanyanon <thanyatle24@hotmail.com>
2023-10-15 21:45:39 +0700

    finish1

--- src/views/MainPageViews.vue
+++ src/views/MainPageViews.vue
@@ -1,5 +1,5 @@
 <script setup>
-import AllProduct from '../components/Allproduct.vue';
+import AllProduct from '../components/AllProduct.vue';
 import SlidePage from '../components/SlidePage.vue';
 </script>
 
